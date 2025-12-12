0f4bd0f6a91b01727aec01008487028cda6209f8
devappsyork <dev.appsyork@gmail.com>
2025-12-12 15:34:24 +0500

    URL update snapchat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Snap Pixel Code --> */}
+        <Script
+          id="26fd5ed2-f829-4d55-a73a-b11fc8858b94"
+          strategy="afterInteractive"
+        >{`
+        (function(e,t,n){
+          if(e.snaptr) return;
+          var a=e.snaptr=function(){
+            a.handleRequest ? a.handleRequest.apply(a,arguments) : a.queue.push(arguments);
+          };
+          a.queue=[];
+          var s='script';
+          var r=t.createElement(s);
+          r.async=true;
+          r.src=n;
+          var u=t.getElementsByTagName(s)[0];
+          u.parentNode.insertBefore(r,u);
+        })(window,document,'https://sc-static.net/scevent.min.js');
+      `}</Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -37,8 +58,13 @@ export default function RootLayout({
         </div>
         <ConsentBanner />
 
-        <script
+        {/* <script
           src="https://events-iq-hub.vercel.app/sdk/pk_live_74ee724b3e5d23a2d5207059.js"
+          async
+        ></script> */}
+
+        <script
+          src="https://23d726edb008.ngrok-free.app/sdk/pk_live_74ee724b3e5d23a2d5207059.js"
           async
         ></script>
       </body>
