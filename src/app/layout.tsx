d8b969e4c46adcf6571ab6a573457cfe41beed29
devappsyork <dev.appsyork@gmail.com>
2026-01-02 15:01:23 +0500

    URL update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
         ></script> */}
 
         <script
-          src="https://427c0763c904.ngrok-free.app/sdk/pk_live_74ee724b3e5d23a2d5207059.js"
+          src="https://94268003f65d.ngrok-free.app/sdk/pk_live_74ee724b3e5d23a2d5207059.js"
           async
         ></script>
       </body>
